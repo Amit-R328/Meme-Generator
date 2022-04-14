8e6c16a45a4716fa8386bb204b7dfa527782c2d8
Amit <amitrozen2015@gmail.com>
2022-04-14 07:15:27 +0300

    .

--- js/services/gallery-service.js
+++ js/services/gallery-service.js
@@ -1,12 +1,24 @@
 'use strict'
 
 let gMemes = [
-    {idx: 0, },
     {idx: 1, },
     {idx: 2, },
     {idx: 3, },
     {idx: 4, },
-    {idx: 5, }
+    {idx: 5, },
+    {idx: 6, },
+    {idx: 7, },
+    {idx: 8, },
+    {idx: 9, },
+    {idx: 10, },
+    {idx: 11, },
+    {idx: 12, },
+    {idx: 13, },
+    {idx: 14, },
+    {idx: 15, },
+    {idx: 16, },
+    {idx: 17, },
+    {idx: 18, },
 ]
 
 function getMemes(){
